de48adc8ce9033a8f1cc5a3fd28b0851929c4a79
Wojciech Wysocki <woytek05@gmail.com>
2022-12-16 08:17:59 +0100

    Add game mechanics

--- memory/Cell.swift
+++ memory/Cell.swift
@@ -3,18 +3,27 @@ import UIKit
 class Cell {
     var button: UIButton
     var image: UIImage
+    var hiddenImage: UIImage
     var tag: Int
+    var active: Bool
+    var uncovered: Bool
     
-    init(image: UIImage, tag: Int) {
+    init(hiddenImage: UIImage, image: UIImage, tag: Int, active: Bool, uncovered: Bool) {
         self.button = UIButton()
+        self.hiddenImage = hiddenImage
         self.image = image
-        self.button.setBackgroundImage(image, for: UIControl.State.normal)
+        self.button.setBackgroundImage(hiddenImage, for: UIControl.State.normal)
         self.tag = tag
         self.button.tag = tag
+        self.active = active
+        self.uncovered = uncovered
     }
     
     func setImage(image: UIImage) {
         self.image = image
+    }
+    
+    func setBackgroundImage(image: UIImage) {
         self.button.setBackgroundImage(image, for: UIControl.State.normal)
     }
     
@@ -25,5 +34,50 @@ class Cell {
     
     func setFrame(frame: CGRect) {
         self.button.frame = frame
+    }
+    
+    func changeImageOnClick() {
+        self.button.addTarget(self, action: #selector(cellClick), for: UIControl.Event.touchUpInside)
+    }
+    
+    @objc func cellClick(sender: UIButton) {
+        self.setBackgroundImage(image: image)
+        self.active = true
+    }
+    
+    func uncoverPermanentlyAndDisable() {
+        self.setBackgroundImage(image: self.image)
+        self.button.isUserInteractionEnabled = false
+        self.active = false
+        self.uncovered = true
+    }
+    
+    func hide(cells: Array<Array<Cell>>) {
+        self.disableCells(cells: cells)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.setBackgroundImage(image: self.hiddenImage)
+        }
+        self.active = false
+        enableCells(cells: cells)
+    }
+    
+    func enableCells(cells: Array<Array<Cell>>) {
+        for row in cells {
+            for cell in row {
+                if !cell.uncovered {
+                    cell.button.isUserInteractionEnabled = true
+                }
+            }
+        }
+    }
+    
+    func disableCells(cells: Array<Array<Cell>>) {
+        for row in cells {
+            for cell in row {
+                if cell.button != self.button {
+                    cell.button.isUserInteractionEnabled = false
+                }
+            }
+        }
     }
 }
--- memory/ViewController2.swift
+++ memory/ViewController2.swift
@@ -12,10 +12,27 @@ class ViewController2: UIViewController {
     let MARGIN = 10.0;
     var level = 0;
     var cells: Array<Array<Cell>> = [[]]
+    var images: Array<UIImage> = Array<UIImage>()
+    var moves = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         selectLevel()
+    }
+    
+    func setImagesArray() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+
+        do {
+            let items = try fm.contentsOfDirectory(atPath: path + "/img")
+
+            for item in items {
+                images.append(UIImage(named: "img/\(item)")!)
+            }
+        } catch {
+            print("Failed to read directory")
+        }
     }
     
     func selectLevel() {
@@ -41,22 +58,45 @@ class ViewController2: UIViewController {
         if (cellSize * nRows >= height) {
             cellSize = (height - ((nRows + 1) * MARGIN)) / nRows
         }
-        var tag = 0
         
         let cellsSpaceHeight = (nRows * cellSize) + ((nRows - 1) * MARGIN)
         let marginTop = ((height - cellsSpaceHeight) / 2) + statusBarHeight + navBarHeight
-        let marginLeft = (width - ((mCols * cellSize) + ((mCols + 1) * MARGIN))) / 2
+        let marginLeft = (width - ((mCols * cellSize) + ((mCols - 1) * MARGIN))) / 2
+        
+        let numOfCells = Int(nRows) * Int(mCols)
+        var tags: Array<Int> = []
+        var tagsAndImages: [Int: UIImage] = [Int: UIImage]()
+        var usedImages: Array<UIImage> = []
+        setImagesArray()
+        
+        for num in 0..<(numOfCells / 2) {
+            tags.append(num)
+            tags.append(num)
+            var randomImage: UIImage = UIImage()
+            repeat {
+                randomImage = images.randomElement()!
+            } while (usedImages.contains(randomImage))
+            tagsAndImages[num] = randomImage
+            usedImages.append(randomImage)
+        }
         
         var it = 0
         if (marginLeft > 0.0) {
             for i in stride(from: 0, to: (cellSize + MARGIN) * nRows, by: (cellSize + MARGIN))  {
                 cells.append([])
                 for j in stride(from: 0, to: (cellSize + MARGIN) * mCols, by: (cellSize + MARGIN)) {
-                    let cell = Cell(image: UIImage(named: "img/none.jpg")!, tag: tag)
+                    tags.shuffle()
+                    let cell = Cell(hiddenImage: UIImage(named: "none/none.jpg")!,
+                                    image: tagsAndImages[tags.last!]!,
+                                    tag: tags.last!,
+                                    active: false,
+                                    uncovered: false)
+                    cell.changeImageOnClick()
+                    cell.button.addTarget(self, action: #selector(lookForPair), for: UIControl.Event.touchUpInside)
+                    tags.removeLast()
                     cell.setFrame(frame: CGRect(x: marginLeft + j, y: marginTop + i, width: cellSize, height: cellSize))
                     self.view.addSubview(cell.button)
                     cells[it].append(cell)
-                    tag += 1
                 }
                 it += 1
             }
@@ -64,25 +104,66 @@ class ViewController2: UIViewController {
             for i in stride(from: 0, to: (cellSize + MARGIN) * nRows, by: (cellSize + MARGIN))  {
                 cells.append([])
                 for j in stride(from: MARGIN, to: (cellSize + MARGIN) * mCols, by: (cellSize + MARGIN)) {
-                    let cell = Cell(image: UIImage(named: "img/none.jpg")!, tag: tag)
+                    tags.shuffle()
+                    let cell = Cell(hiddenImage: UIImage(named: "none/none.jpg")!,
+                                    image: tagsAndImages[tags.last!]!,
+                                    tag: tags.last!,
+                                    active: false,
+                                    uncovered: false)
+                    cell.changeImageOnClick()
+                    cell.button.addTarget(self, action: #selector(lookForPair), for: UIControl.Event.touchUpInside)
+                    tags.removeLast()
                     cell.setFrame(frame: CGRect(x: j, y: marginTop + i, width: cellSize, height: cellSize))
                     self.view.addSubview(cell.button)
                     cells[it].append(cell)
-                    tag += 1
                 }
                 it += 1
             }
         }
-
-        for row in cells {
-            for cell in row {
-                cell.button.addTarget(self, action: #selector(jazda), for: UIControl.Event.touchUpInside)
-            }
-        }
     }
     
-    @objc func jazda(sender: UIButton) {
-        print(sender.tag)
+    @objc func lookForPair(sender: UIButton) {
+        moves += 1
+        var gameOver = true
+        var activeCells: Array<Cell> = []
+        for row in cells {
+            for cell in row {
+                if cell.active {
+                    activeCells.append(cell)
+                }
+            }
+        }
+        
+        if activeCells.count == 2 {
+            if activeCells[0].tag == activeCells[1].tag {
+                activeCells[0].uncoverPermanentlyAndDisable()
+                activeCells[1].uncoverPermanentlyAndDisable()
+            } else {
+                activeCells[0].hide(cells: cells)
+                activeCells[1].hide(cells: cells)
+            }
+        }
+        
+        for row in cells {
+            for cell in row {
+                if !cell.uncovered {
+                    gameOver = false
+                }
+            }
+        }
+        
+        if (gameOver) {
+            let gameOverAlert = UIAlertController(title: "Koniec!",
+                                                  message: "Ilość ruchów: \(moves)",
+                                                  preferredStyle: .alert)
+            gameOverAlert.addAction(UIAlertAction(title: "Powrót",
+                                                  style: .default,
+                                                  handler: { action in
+                self.navigationController?.popToRootViewController(animated: true)
+            }))
+            
+            self.present(gameOverAlert, animated: true)
+        }
     }
     
     func reframeGrid(width: Double, height: Double, nRows: Double, mCols: Double) {
@@ -90,16 +171,15 @@ class ViewController2: UIViewController {
         let navBarHeight = self.navigationController!.navigationBar.bounds.height
         var cellSize = (width - ((mCols + 1) * MARGIN)) / mCols
         if (cellSize * nRows >= height) {
-            cellSize = (height - ((nRows - 1) * MARGIN)) / nRows
+            cellSize = (height - ((nRows + 1) * MARGIN)) / nRows
         }
         
         let cellsSpaceHeight = (nRows * cellSize) + ((nRows - 1) * MARGIN)
         let marginTop = ((height - cellsSpaceHeight) / 2) + statusBarHeight + navBarHeight
-        let marginLeft = (width - ((mCols * cellSize) + ((mCols + 1) * MARGIN))) / 2
+        let marginLeft = (width - ((mCols * cellSize) + ((mCols - 1) * MARGIN))) / 2
         
         var it_1 = 0
         var it_2 = 0
-        
         if (marginLeft > 0.0) {
             for i in stride(from: 0, to: (cellSize + MARGIN) * nRows, by: (cellSize + MARGIN))  {
                 cells.append([])
